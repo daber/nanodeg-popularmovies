8638670af34ed2fe0f2aaf7ee463c6cab99aba89
Michal Dabrowski <m.a.dabrowski@gmail.com>
2016-10-16 22:34:52 +0200

    Added no network case

--- app/src/main/java/com/abitcreative/popularmovies/async/NetworkAsync.kt
+++ app/src/main/java/com/abitcreative/popularmovies/async/NetworkAsync.kt
@@ -1,14 +1,21 @@
 package com.abitcreative.popularmovies.async
 
 import android.os.AsyncTask
+import android.util.Log
 import retrofit2.Call
 
 /**
  * Created by daber on 15/10/16.
  */
 open class NetworkAsync<T>(val call: Call<T>, val onResult: (T?) -> Unit) : AsyncTask<Void, Void, T?>() {
-    override fun doInBackground(vararg params: Void?): T {
-        return call.execute().body()
+    val TAG = javaClass.simpleName
+    override fun doInBackground(vararg params: Void?): T? {
+        try {
+            return call.execute().body()
+        } catch (e: Exception) {
+            Log.e(TAG, "Exception while making request", e)
+            return null
+        }
     }
 
     override fun onPostExecute(result: T?) {
